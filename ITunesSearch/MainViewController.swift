34d65004eae04907acbbbb1ea12f52c29e56872f
seongmin kim <k2417000@naver.com>
2024-08-08 15:12:35 +0900

    초기 설정 - gitignore, 최소 버전, 라이트모드, 세로모드, codebase, add package dependencies

--- ITunesSearch/MainViewController.swift
+++ ITunesSearch/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  ITunesSearch
 //
 //  Created by 김성민 on 8/8/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
