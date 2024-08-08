d8c556606d2dd4d7638cdb483859e5236052ddaa
seongmin kim <k2417000@naver.com>
2024-08-08 15:07:42 +0900

    Initial Commit

---
+++ ITunesSearch/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ITunesSearch
+//
+//  Created by 김성민 on 8/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
